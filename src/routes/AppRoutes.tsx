9e9b1bd481d57a745346a4258a6fb6f553bc8abd
Tống Trần Lê Khoa <noodle.professor@gmail.com>
2026-06-16 17:17:55 +0700

    check token

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
-import RegisterCar from "../pages/customer/RegisterCar";
+import Register from "../pages/auth/Register";
 import LoginedHomePage from "../pages/customer/LoginedHomePage";
+import RegisterCar from "../pages/customer/RegisterCar";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Trang public */}
         <Route path="/" element={<HomePage />} />
-
-        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route path="/home" element={<LoginedHomePage />} />
-        <Route path="/register-car" element={<RegisterCar />} />
+        {/* Trang cần đăng nhập */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <LoginedHomePage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/register-car"
+          element={
+            <ProtectedRoute>
+              <RegisterCar />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
